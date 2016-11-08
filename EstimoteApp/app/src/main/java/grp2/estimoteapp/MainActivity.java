44509fa409b448ffa0734612e8b16ab2d15ea93a
smellmee <smellmee@hotmail.com>
2016-11-08 12:06:03 +0200

    Revert "Revert "Revert "Estimoten käyttöön ottoa"""
    This reverts commit c8cb1e01542c8548c1374a10fbd02a35261bd1ff.

--- EstimoteApp/app/src/main/java/grp2/estimoteapp/MainActivity.java
+++ EstimoteApp/app/src/main/java/grp2/estimoteapp/MainActivity.java
@@ -3,22 +3,11 @@ package grp2.estimoteapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.estimote.sdk.SystemRequirementsChecker;
-
 public class MainActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    //ask the user to turn Bluetooth or Location on, or ask for ACCESS_COARSE_LOCATION permission.
-    @Override
-    protected void onResume() {
-        super.onResume();
-        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 }
