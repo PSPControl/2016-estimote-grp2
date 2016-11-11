f387493c7aef64c8ea4653b2fec31899848b0252
Petri Virkkunen <pepejovi@gmail.com>
2016-11-11 16:32:26 +0200

    Basic website functions
    Receives data from PHP, just need to make the PHP receive data. And send
    it to the database i guess.

---
+++ www/Beacon/php/data.php
@@ -0,0 +1,13 @@
+<?php
+	$tempData = "Temperature data goes here";
+	$coords = "Co-ordinates go here";
+	$UUID = "UUID goes here";
+?>
+
+<html>
+	<pre>
+	Lämpötila: <?php echo $tempData . "\n"; ?>
+	UUID: <?php echo $UUID . "\n"; ?>
+	Koordinaatit: <?php echo $coords . "\n"; ?>
+	</pre>
+</html>
